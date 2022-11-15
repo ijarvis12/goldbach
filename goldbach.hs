d4449f9fc8123d75670871f507308bdbc20e9334
Ian Jarvis <ijarvis12@gmail.com>
2022-11-15 15:19:24 -0600

    changed isPrime

--- goldbach.hs
+++ goldbach.hs
@@ -1,17 +1,22 @@
 #!/usr/bin/runhaskell
 
-isPrime :: Integer -> Bool
-isPrime 1 = False
-isPrime 2 = True
-isPrime n | (length [x | x <- [2 .. (toInteger (ceiling (sqrt (fromIntegral n))))], mod n x == 0]) > 0 = False
-          | otherwise = True
+
+check :: Integer -> Integer -> Bool
+check x n = (mod x n) == 0
+
+isPrime :: Integer -> Integer -> Bool -> Bool
+isPrime x 1 False = True
+isPrime x end False = isPrime x (end-1) (check x end)
+isPrime x end True = False
 
 forLoop :: Integer -> Integer -> Bool -> Bool
 forLoop 1 b False = False
 forLoop a b True = True
 forLoop a b False = do
-    let x = isPrime a
-    let y = isPrime b
+    let enda = (toInteger (ceiling (sqrt (fromIntegral a))) + 1)
+    let endb = (toInteger (ceiling (sqrt (fromIntegral b))) + 1)
+    let x = isPrime a enda False
+    let y = isPrime b endb False
     forLoop (a-1) (b+1) (x==y)
 
 goldbach :: Integer -> Bool -> Maybe Bool
