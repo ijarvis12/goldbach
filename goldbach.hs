167ba62b4343f4d60c687de41ea627d8bdb4162d
Ian Jarvis <ijarvis12@gmail.com>
2023-09-20 13:37:12 -0500

    Update goldbach.hs
    actually output numbers

--- goldbach.hs
+++ goldbach.hs
@@ -5,16 +5,15 @@ isPrime :: Integer -> Bool
 isPrime n = null [y | y <- [2..(ceiling (sqrt (fromInteger n)))], mod n y == 0]
 
 -- check for an x of goldbach, all addtions to get x there is a prime pair, if not return the number
-forLoop :: [Integer] -> Integer -> Bool -> Bool -> Maybe Integer
-forLoop (a:as) b False True = Just (a+b)
-forLoop (a:as) _ True _ = forLoop as 2 False False
+forLoop :: [Integer] -> Integer -> Bool -> Bool -> IO ()
+forLoop (a:as) b False True = print (a+b)
+forLoop (a:as) b True _ = do {print [(a+b),(a+1),(b-1)]; forLoop as 2 False False}
 forLoop (a:as) b False False = forLoop ([a-1]++as) (b+1) ((isPrime a) && (isPrime b)) (b>a)
 
 -- the goldbach conjecture for all even integers (to a stopping point?)
-goldbach :: Maybe Integer -> Integer
-goldbach Nothing = goldbach (forLoop [(4*10^18),(4*10^18+2)..] 2 False False)
-goldbach (Just x) = x
+goldbach :: IO ()
+goldbach = forLoop [(4*10^12),(4*10^12+2)..] 2 False False
 
 -- main function
 main :: IO ()
-main = print (goldbach Nothing)
+main = goldbach
